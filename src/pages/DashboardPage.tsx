de0a028e896f77c54af352ad2e6d649db80880f7
cbussalleu <100375866+cbussalleu@users.noreply.github.com>
2025-03-20 15:16:31 +0100

    Create DashboardPage.tsx

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Dashboard from '@/components/Dashboard';
+
+/**
+ * Dashboard principal
+ */
+const DashboardPage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-dark">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default DashboardPage;
